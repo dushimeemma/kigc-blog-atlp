e705fb04b05afd5d72fe3c3c6c013d6842d0da85
Dushime Emmanuel <dushimeemma@gmail.com>
2020-08-06 15:40:16 +0200

    chore(createComment): implement create comment
    -add comment to an article
    
    [Delivers #64]

--- UI/js/singleblog.js
+++ UI/js/singleblog.js
@@ -14,20 +14,81 @@ articleRef.on('value', (snapshot) => {
   article.classList.add('border-radius');
   article.classList.add('p');
   article.classList.add('mt');
-  article.innerHTML = `          <h3 class="m-bottom capitalize">${
-    snapshot.val().title
-  }</h3>
-  <p class="m-bottom">
-   ${snapshot.val().body}
-  </p>
-  <h6 class="text-center capitalize m-bottom">leave your comment here</h6>
-  <form class="mb">
-    <div class="form-group">
-      <textarea name="" id="" cols="10" rows="3">
-Your Comment Here ...</textarea
-      >
-    </div>
-    <button class="float-right mb">Comment</button>
-  </form>`;
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      article.innerHTML = `          <h3 class="m-bottom capitalize">${
+        snapshot.val().title
+      }</h3>
+      <p class="m-bottom">
+       ${snapshot.val().body}
+      </p>
+      <h6 class="text-center capitalize m-bottom">leave your comment here</h6>
+      <div class="errors text-center" id="errors"></div>
+      <form class="mb" id="form-comment">
+        <div class="form-group">
+          <textarea name="comment" cols="10" rows="3" id="comment"></textarea
+          >
+        </div>
+        <img src="./images/viewcomment.png" alt="view comment" class="icon m-icon i-class" id="view" /><img src="./images/close.png" alt="view comment" class="icon m-icon i-class errors" id="close-comment" /> <button class="float-right mb">Comment</button>
+      </form>
+<div class="comment text-black" id="comment-area">
+    <h3 class="text-center">Comments</h3> <hr/>
+</div>`;
+    } else {
+      article.innerHTML = `<h3 class="m-bottom capitalize">${
+        snapshot.val().title
+      }</h3>
+      <p class="m-bottom">
+       ${snapshot.val().body}
+      </p>`;
+    }
+    //allow user to leave a comment
+    let formComment = document.querySelector('#form-comment');
+    let comment = formComment['comment'];
+    let errors = document.querySelector('#errors');
+
+    formComment.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (comment.value === '') {
+        errors.style.display = 'block';
+        errors.innerHTML = 'blank comment is not allowed';
+        setTimeout(() => errors.remove(), 5000);
+        formComment.reset();
+      } else {
+        firebase
+          .database()
+          .ref(`comments/${id}`)
+          .push()
+          .set({
+            comment: comment.value,
+          })
+          .then(() => {
+            errors.style.display = 'block';
+            errors.innerHTML = '<p class="success">comment send</p>';
+            setTimeout(() => errors.remove(), 5000);
+            formComment.reset();
+          });
+      }
+    });
+    //show comments
+    document.querySelector('#view').addEventListener('click', (e) => {
+      let com = document.querySelector('#comment-area');
+      com.style.display = 'block';
+      firebase
+        .database()
+        .ref(`comments/${id}`)
+        .on('child_added', (snapshot) => {
+          let p = document.createElement('p');
+          p.innerHTML = `&middot;${snapshot.val().comment}<hr/>`;
+          com.appendChild(p);
+          count++;
+        });
+      document.querySelector('#view').addEventListener('click', (e) => {
+        com.style.display = 'none';
+        window.location.href = 'singleblog.html?id=' + id;
+      });
+    });
+  });
+
   document.querySelector('#articles').appendChild(article);
 });
